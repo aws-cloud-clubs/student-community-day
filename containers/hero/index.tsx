25754ab61dd2e30ac95228d8ac9158f0e6406180
Changhyeon Yoon <hyeon120411@gmail.com>
2024-02-21 23:07:41 +0900

    Feat : Hero mobile UI

--- containers/hero/index.tsx
+++ containers/hero/index.tsx
@@ -5,20 +5,27 @@ export const HeroSection: React.FC = () => {
   return (
     <div id='hero' className='w-screen flex relative'>
       <Image
-        src='/assets/images/hero-object.png'
+        src='/assets/images/object.png'
         alt='animation object'
         width={1280}
         height={720}
-        className='absolute top-0 left-0 z-0 w-screen h-screen z-1 animate-bounce max-md:hidden'
+        className='absolute top-0 left-0 z-0 w-screen h-full z-1 animate-bounce '
       />
       {/* MEMO Typo bg image에서 분리하기*/}
       <Image
         priority
-        src={"/assets/images/hero-bg.png"}
+        src={"/assets/images/background.png"}
         alt='background image'
         width={1280}
         height={720}
-        className='md:w-screen md:h-screen max-md:h-1/2'
+        className='w-screen h-screen object-cover '
+      />
+      <Image
+        src={"/assets/images/typo.png"}
+        alt='typo'
+        width={797}
+        height={364}
+        className='absolute top-0 left-0 bottom-0 right-0 m-auto z-20 object-contain max-md:px-5'
       />
     </div>
   );
--- containers/location/index.tsx
+++ containers/location/index.tsx
@@ -5,13 +5,15 @@ export const LocationSection: React.FC = () => {
   return (
     <div
       id='location'
-      className='w-full h-full flex items-center bg-white px-10 py-40 gap-y-10 max-md:py-10 max-md:gap-y-5 justify-center gap-x-20'
+      className='w-full h-full flex items-center bg-white px-10 py-40 gap-y-10 max-md:py-10 max-md:gap-y-5 justify-center gap-x-20 max-md:flex-col'
     >
-      <div className='flex flex-col'>
-        <p className='text-5xl font-bold whitespace-pre-wrap leading-snug'>{`4월 6일,\n우리 함께 만나요`}</p>
-        <p className='text-gray-400 mt-1'>* 대중교통 이용을 권장드려요</p>
+      <div className='flex flex-col max-md:items-center max-md:gap-x-2.5'>
+        <div className='flex flex-col max-md:mb-5'>
+          <p className='text-5xl font-bold whitespace-pre-wrap leading-snug max-md:text-3xl max-md:text-center'>{`4월 6일,\n우리 함께 만나요`}</p>
+          <p className='text-gray-400 mt-1 max-md:text-md max-md:text-center'>* 대중교통 이용을 권장드려요</p>
+        </div>
         <a
-          className='py-2 w-28 border border-gray-900 rounded-full font-semibold mt-5 text-center flex justify-center items-center gap-x-1'
+          className='py-2 w-28 border border-gray-900 rounded-full font-semibold mt-5 text-center flex justify-center items-center gap-x-1 max-md:mt-0'
           target='_blank'
           href='https://kko.to/DFNgReJc7O'
         >
@@ -19,10 +21,10 @@ export const LocationSection: React.FC = () => {
           <span>길찾기</span>
         </a>
       </div>
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex justify-center items-center rounded-5xl overflow-hidden'>
         <a href='https://kko.to/DFNgReJc7O' target='_blank'>
           <img
-            className='w-[504px] h-[310px]'
+            className='w-[504px] h-[310px] '
             src='https://map2.daum.net/map/mapservice?FORMAT=PNG&amp;SCALE=2.5&amp;MX=509203&amp;MY=1112253&amp;S=0&amp;IW=504&amp;IH=310&amp;LANG=0&amp;COORDSTM=WCONGNAMUL&amp;logo=kakao_logo'
           />
         </a>
