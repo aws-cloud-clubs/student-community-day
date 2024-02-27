f5693a938e73d14254eeb7c3269dc49f6312b27d
Changhyeon Yoon <hyeon120411@gmail.com>
2024-02-27 18:14:19 +0900

    Fix root

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { useRouter } from "next/router";
 import React from "react";
 
 export default function Home() {
-  const router = useRouter();
-  React.useEffect(() => {
-    router.replace("/2024");
-  }, []);
   return <></>;
 }
