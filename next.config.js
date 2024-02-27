1f81fd88e0af5be95a37b60eeffed8a22c860804
Changhyeon Yoon <hyeon120411@gmail.com>
2024-02-27 17:52:05 +0900

    Fix : assetprefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import("next").NextConfig} */
-const BaseURL = "https://aws-cloud-clubs.github.io/student-community-day";
+const BaseURL = "https://aws-cloud-clubs.github.io/student-community-day/";
 const nextConfig = {
   output: "export",
   distDir: "build",
   reactStrictMode: true,
-  basePath: "/student-community-day",
   assetPrefix: BaseURL,
   images: {
     loader: "imgix",
