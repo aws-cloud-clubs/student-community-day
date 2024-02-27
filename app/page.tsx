d02b1c4c6c755601c126e1d2927e99060e19a1ed
Changhyeon Yoon <hyeon120411@gmail.com>
2024-02-27 18:04:10 +0900

    Fix : Redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Home() {
+  const router = useRouter();
+  React.useEffect(() => {
+    router.replace("/2024");
+  }, []);
   return <></>;
 }
